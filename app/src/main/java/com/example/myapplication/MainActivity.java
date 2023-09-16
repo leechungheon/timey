e3894e43a373b3f94a8f84d628015a8ea4c00965
dawn_lch <chung4111@naver.com>
2023-09-16 15:35:03 +0900

    timey

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onButton1Clicked(View view){
-        Toast.makeText(this,"버튼1이 눌렸어요.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
+    }
+    public void onButton3Clicked(View view){
+        Intent myIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
     }
 
 }
